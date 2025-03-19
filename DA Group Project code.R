60b7754ad832dc48e2e45d78e1ecb5dc23c4bd88
Abbeyod <2764127o@student.gla.ac.uk>
2025-03-19 13:01:07 +0000

    Create DA Group Project code.R

---
+++ DA Group Project code.R
@@ -0,0 +1,22 @@
+data <- read.csv("DAProject6.csv")
+
+library(tidyverse)
+library(tidymodels)
+library(moderndive)
+library(skimr)
+library(gapminder)
+
+
+
+model1 <- linear_reg() |>
+  fit(BMI ~ Year, data = data)
+model1
+
+
+regression_points <- get_regression_points(model1$fit)
+regression_points
+
+ggplot(regression_points, aes(x = Year, y = residual)) +
+  geom_jitter(width = 0.1) + 
+  labs(x = "Year", y = "Residual") +
+  geom_hline(yintercept = 0, col = "blue")
